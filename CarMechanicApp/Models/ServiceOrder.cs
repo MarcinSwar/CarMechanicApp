c84e94da991a7bf132a5b01234e0b502f1d9ae4f
Marcin S <marcin600rr@hotmail.com>
2019-01-29 00:14:51 +0100

    new model POCOs added

---
+++ CarMechanicApp/Models/ServiceOrder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarMechanicApp.Models
+{
+    class ServiceOrder
+    {
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public Mechanic Mechanic { get; set; }
+        [Required]
+        public int MechanicId { get; set; }
+
+        public Vehicle Vehicle { get; set; }
+        [Required]
+        public int VehicleId { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+    }
+}
--- CarMechanicApp/Models/Vehicle.cs
+++ CarMechanicApp/Models/Vehicle.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,9 @@ namespace CarMechanicApp.Models
 {
     class Vehicle
     {
+
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Required]
         public int Id { get; set; }
 
